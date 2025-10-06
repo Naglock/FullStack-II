0f54d00de9db407a3e24f55e5cac9102ececf736
Naglock <iv.huentemilla@duocuc.cl>
2025-10-06 14:58:07 -0300

    modificaciones clase 6-10

--- react-responsivo/src/App.jsx
+++ react-responsivo/src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import './App.css';
 import Controls from './components/Controls';
 import MemeCanvas from './components/MemeCanvas';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   const [image, setImage] = useState(null);
@@ -20,6 +24,13 @@ function App() {
         topText={topText}
         bottomText={bottomText}
       />
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} /> 
+      </Routes>
+      <Footer />
     </>
   );
 }
---
+++ react-responsivo/src/components/Header.jsx
@@ -0,0 +1,6 @@
+import { Link } from 'react-router-dom';
+<nav>
+    <Link to='/'>Inicio</Link>
+    <Link to='/about'>Acerca</Link>
+    <Link to='/contact'>Contacto</Link>
+</nav>
