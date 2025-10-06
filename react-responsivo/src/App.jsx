ae72141e45de83b61aa2f456bde03b5923ec6719
Naglock <iv.huentemilla@duocuc.cl>
2025-10-06 14:59:39 -0300

    cambios

--- react-responsivo/src/App.jsx
+++ react-responsivo/src/App.jsx
@@ -1,17 +1,10 @@
-import { useState } from 'react';
 import './App.css';
-import Controls from './components/Controls';
-import MemeCanvas from './components/MemeCanvas';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 
 function App() {
-  const [image, setImage] = useState(null);
-  const [topText, setTopText] = useState('');
-  const [bottomText, setBottomText] = useState('');
-
   return (
     <>
       <Header />
